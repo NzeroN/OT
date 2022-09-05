937266188f864d32b9a71d60de747052389410b0
Bun <bunpatta@gmail.com>
2022-09-05 11:34:53 +0700

    ready to perform main menu

--- overtime/Assets/scripts/DataPersistants/Data/GameData.cs
+++ overtime/Assets/scripts/DataPersistants/Data/GameData.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class GameData : MonoBehaviour
+public class GameData
 {
-    public float position;
-
     public int deathCount;
+    public Vector3 playerPosition;
+    public SerializableDictionary<string, bool> coinsCollected;
 
+    // the values defined in this constructor will be the default values
+    // the game starts with when there's no data to load
     public GameData()
     {
         this.deathCount = 0;
-        this.position = 0;
+        playerPosition = Vector3.zero;
+        coinsCollected = new SerializableDictionary<string, bool>();
     }
 }
