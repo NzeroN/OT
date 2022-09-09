c560be53bfc0bb9140b8315614896a814bcc1bf7
Bun <bunpatta@gmail.com>
2022-09-09 14:52:58 +0700

    fix main menu

--- overtime/Assets/scripts/DataPersistants/Data/GameData.cs
+++ overtime/Assets/scripts/DataPersistants/Data/GameData.cs
@@ -9,7 +9,6 @@ public class GameData
     public int deathCount;
     public Vector3 playerPosition;
     public SerializableDictionary<string, bool> coinsCollected;
-    public AttributesData playerAttributesData;
 
     // the values defined in this constructor will be the default values
     // the game starts with when there's no data to load
@@ -18,7 +17,6 @@ public class GameData
         this.deathCount = 0;
         playerPosition = Vector3.zero;
         coinsCollected = new SerializableDictionary<string, bool>();
-        playerAttributesData = new AttributesData();
     }
 
     public int GetPercentageComplete()
